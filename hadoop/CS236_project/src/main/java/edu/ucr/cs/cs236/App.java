8ffa0c0caaf5b4ff2256721d35d32cd5950c8262
kaushikdaiv7 <kaushik777d@gmail.com>
2023-12-25 11:37:39 -0800

    Project added to Github

--- hadoop/CS236_project/src/main/java/edu/ucr/cs/cs236/App.java
+++ hadoop/CS236_project/src/main/java/edu/ucr/cs/cs236/App.java
@@ -1,8 +1,6 @@
 package edu.ucr.cs.cs236;
-
 import java.io.IOException;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -13,6 +11,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+
 public class App {
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable> {
@@ -21,33 +20,29 @@ public class App {
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            // one row in the CSV, in a string
+
             String currentLine = value.toString();
-            // the string split by commas 
+
             String[] tokens = currentLine.split(",");
+            
+            //Setting group to the trimmed State value 
+            String group = tokens[1].trim(); 
 
-            //TODO: Put a string value to group by. Note how the group variable is used in the rest of the function
-            // How will you get the group-by value?
-            String group = ""; 
+            //Filter rows with variable code
+            String variable_to_sum = "2010_Census_Population";
 
-            // TODO: Put the column that you are summing
-            // Where do you use this? Think about your SQL query that you wrote for PySpark
-            String variable_to_sum = "";
-
-            // TODO: Change the condition to filter to make sure you're summing up only the values that you want
-            if (true){
+            if (tokens[3].equals(variable_to_sum)){
                 try {
-                    // TODO: Parse the correct token into an integer
-                    // it is okay to hard-code the index of tokens
-                    // Use Integer.parseInt() to do this
-                    // int colValue = ____;
+                    
+                    //Type cast the population value to integer
+                    int colValue = Integer.parseInt(tokens[4]);
 
-                    // TODO: What will the groupKey be? This is how Hadoop knows what to sum together
-                    // use `groupKey.set(____);`
+                    //Set key as State
+                    groupKey.set(group);
 
-                    // TODO: What value will you be pairing with the key? This is the value that Hadoop is summing
-                    // outputValue.set(_____);
-
+                    //Set Value as the population of County
+                    outputValue.set(colValue);
+                    
                     context.write(groupKey, outputValue);
                 } catch (NumberFormatException e) {
                     // leave this empty
@@ -65,10 +60,8 @@ public class App {
         ) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
-                // TODO: values is an iterable that contains all the values pertaining to the key 
-                // How do we get the total value?
-                
-                // `sum = ____`;
+                //Sum the population of counties for each state
+                sum += val.get();
             }
             result.set(sum);
             context.write(key, result);
@@ -76,6 +69,8 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
+        long startTime = System.nanoTime();
+        System.out.println("Start time: " + startTime);
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "get_state_by_pop_2010");
         job.setJarByClass(App.class);
@@ -86,6 +81,11 @@ public class App {
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        boolean success = job.waitForCompletion(true);
+        long endTime = System.nanoTime();
+        System.out.println("Time required: " + endTime);
+        double timeRequired = (endTime - startTime) / 1e9;
+        System.out.printf("Time required: %.4f seconds\n", timeRequired);
+        System.exit(success ? 0 : 1);
     }
 }
